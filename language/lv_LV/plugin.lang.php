daae901b624aeaf4a60ec925a38b13ca5340eace
ddtddt <ddtddt@mpiwigo.org>
2011-10-03 21:20:06 +0000

    [extensions] - Comments_Access_Manager - add LV (Latvian - Latviešu) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@12337 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['CM_submit'] = 'Saglabāt iestatījumus';
+$lang['CM_save_config'] = 'Konfigurācija sekmīgi saglabāta !';
+$lang['CM_Title'] = 'Komentāru Piekļuves menedžeris - Versija : ';
+$lang['CM_SubTitle'] = 'Spraudņa konfigurācija';
+$lang['CM_Disable'] = ' Atspējot (pēc noklusēšanas)';
+$lang['CM_Enable'] = ' Iespējot ';
+$lang['CM_No_Anonymous_Comments'] = 'Viesu komentāriem ir obligāts Nickname (iesauka)';
+$lang['CM_Comments_For_Group'] = 'Atļaut izdarīt komentārus lietotāju grupai';
+$lang['CM_AllowedComm_Group'] = 'Atlasīt lietotāju grupu, kurai atļauts izdarīt komentārus:';
+$lang['CM_Not_Allowed_Author'] = 'Atvainojiet, jūs neesat autorizējies, lai ievietotu komentāru. Lūdzu kontaktējiet ar vietnes administratoru.';
+$lang['CM_Support_txt'] = 'Oficiālais ši spraudņa atbalsts ir pieejams tikai šajās Piwigo foruma tēmās:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Forums angļu valodā - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Pieejams arī projekta tēmu meklētājs: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Empty Author'] = 'Nepieciešams Komentāra autora nickname !';
+$lang['CM_Validation_For_Group'] = 'Atļaut komentārus bez validācijas pārbaudes';
+$lang['CM_ValidComm_Group'] = 'Atlasīt lietotāju grupu, kuriem atļauts izdarīt komentārus bez validācijas:';
+$lang['CM_CommentsForAll'] = '"Komentēt Visu" opcija ir <b style="color:red;">iespējota</b>';
+$lang['CM_CommentsForRegistered'] = '" Komentēt Visu " opcija ir <b style="color:red;">atspējota</b>';
+$lang['CM_CommentsValidationOn'] = '"Validācija" priekš komentāriem ir <b style="color:red;">iespējota</b>';
+$lang['CM_CommentsValidationOff'] = '" Validācija " priekš komentāriem ir <b style="color:red;">atspējota</b>';
+?>
