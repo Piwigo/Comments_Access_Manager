3bb4bc7a80d260593d143f7b023721fb9ceb471f
ddtddt <ddtddt@mpiwigo.org>
2012-02-08 19:53:44 +0000

    [extensions] - Comments_Access_Manager - add es_ES - Español – Spanish - Thanks to jpr928
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13061 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['CM_submit'] = 'Guardar configuración';
+$lang['CM_save_config'] = '¡ Configuración guardado con éxito !';
+$lang['CM_Title'] = 'Comments Access Manager - Version: ';
+$lang['CM_SubTitle'] = 'configuración del plugin';
+$lang['CM_Disable'] = ' Desactivado (predeterminado)';
+$lang['CM_Enable'] = ' activar ';
+$lang['CM_Comments_For_Group'] = 'Permitir comentarios a un grupo de usuarios';
+$lang['CM_AllowedComm_Group'] = 'Seleccione el grupo de usuarios que pueden publicar comentarios :';
+$lang['CM_Not_Allowed_Author'] = 'Lo sentimos, usted no está autorizado para publicar un comentario. Por favor, póngase en contacto con El administrador del sitio.';
+$lang['CM_Empty Author'] = 'El nombre del autor del comentario es obligatorio !';
+$lang['CM_No_Anonymous_Comments'] = 'Apodo obligatorio para los no registrados';
+$lang['CM_Validation_For_Group'] = 'Permitir comentarios sin la aprobación de un administrador';
+$lang['CM_ValidComm_Group'] = 'Seleccione el grupo de usuarios que pueden publicar comentarios, sin validación :';
+$lang['CM_CommentsForAll'] = 'La opcion "Comentarios para todos" esta<b style="color:red;">activada</b>';
+$lang['CM_CommentsForRegistered'] = 'La opcion "Comentarios para todos" esta <b style="color:red;">desactivada</b>';
+$lang['CM_CommentsValidationOn'] = 'La opción "validación" de los comentarios esta<b style="color:red;">activada</b>';
+$lang['CM_CommentsValidationOff'] = 'La opción "validación" de los comentarios esta<b style="color:red;">desactivada</b>';
+?>
