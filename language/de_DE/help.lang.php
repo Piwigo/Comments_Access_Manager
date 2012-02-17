ed274e5ac217a28e2a91bff974a340037a928bba
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-17 18:58:45 +0000

    [Comments Access Manager] Update de_DE, thanks to : Eric
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13223 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/help.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Validation&quot; für Kommentare ist Deaktiviert';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Diese Option aktivieren, um zu hier sehen die erweiterten Parameter.';
+$lang['CM_commentTitle'] = 'Nickname obligatorisch für Gäste Kommentare';
+$lang['CM_GroupCommTitle'] = 'Kommentaren zulassen zu einer Benutzern Gruppe';
+$lang['CM_ValidCommTitle'] = 'Kommentare zulassen ohne Validierung durch einen Administrator';
+?>
---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Wählen der Benutzern Gruppe gestattet, Kommentare anzulegen:';
+$lang['CM_CommentsForAll'] = '"Kommentare für alle"-Option <b style="color:red;">ist aktiviert</b>';
+$lang['CM_CommentsForRegistered'] = '"Kommentare für alle"-Option <b style="color:red;">ist Deaktiviert</b>';
+$lang['CM_CommentsValidationOff'] = '"Validation" für Kommentare <b style="color:red;">ist Deaktiviert</b>';
+$lang['CM_CommentsValidationOn'] = '"Validation" für Kommentare <b style="color:red;">ist aktiviert</b>';
+$lang['CM_Comments_For_Group'] = 'Kommentaren zulassen zu einer Benutzern Gruppe';
+$lang['CM_Disable'] = 'Deaktivieren (Standard)';
+$lang['CM_Empty Author'] = 'Kommentare Autor nickname ist zwingend erforderlich!';
+$lang['CM_Enable'] = 'aktivieren';
+$lang['CM_Not_Allowed_Author'] = 'Leider sind Sie nicht berechtigt, um einen Kommentar abzugeben. Bitte kontaktieren Sie den Administrator der Website.';
+$lang['CM_No_Anonymous_Comments'] = 'Nickname obligatorisch für Gäste Kommentare';
+$lang['CM_save_config'] = 'Konfiguration erfolgreich gespeichert!';
+$lang['CM_submit'] = 'Einstellungen Speichern';
+$lang['CM_SubTitle'] = 'Plugin-Konfiguration';
+$lang['CM_Support_txt'] = 'Die offizielle Unterstützung auf diesem Plugin ist nur auf diesen Piwigo Forum Thema: <br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Englisch-Forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br>Auch erhältlich, des Projekts Bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;"> http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Comments Access Manager - Version :';
+$lang['CM_Validation_For_Group'] = 'Kommentare zulassen ohne Validierung durch einen Administrator';
+$lang['CM_ValidComm_Group'] = 'Wählen Sie der Benutzern Gruppe gestattet, Kommentare ohne Validierung posten:';
+?>
