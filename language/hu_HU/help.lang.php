7c93c9f325accc9a4d69e36884a0c5bc6736e1fb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-18 15:54:22 +0000

    [Comments Access Manager] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13230 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/help.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_commentTitle'] = 'A hozzászóláshoz a vendégeknek is kötelező felhasználónév megadása';
+$lang['CM_commentTitle_d'] = 'Amennyiben a <b>&quot;Bárki hozzászólhat&quot;</b> opció aktív (nem regisztrált látogatók is hozzászólhatnak), ezzel a beállítással megkövetelheti, hogy a vendégek is adjanak meg felhasználónevet a hozzászólásukhoz.';
+$lang['CM_GroupCommTitle'] = 'Csoport tagok hozzászólásainak engedélyezése';
+$lang['CM_GroupCommTitle_d'] = 'Amennyiben a galéria úgy van beállítva, hogy ne szólhasson hozzá mindenki, ezzel a beállítással engedélyezheti bizonyos csoport tagjainak a hozzászólást.
+<br><br>
+Alapesetben a <b>&quot;Bárki hozzászólhat&quot;</b> opció inaktív, csak regisztrált felhasználók szólhatnak hozzá. Ezt a viselkedést letilthatjuk ezzel a funkcióval, így regisztrált felhasználók és egy bizonyos csoport tagjainak engedélyezi a hozzászólást.';
+?>
---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Válassza ki mely csoporthoz tartozó felhasználók írhatnak hozzászólásokat';
+$lang['CM_CommentsForAll'] = '"Bárki hozzászólhat" opció <b style="color:red;">bekapcsolva</b>';
+$lang['CM_CommentsForRegistered'] = '"Bárki hozzászólhat" opció <b style="color:red;">kikapcsolva</b>';
+$lang['CM_CommentsValidationOff'] = '"Engedélyezés szükséges" opció <b style="color:red;">kikapcsolva</b>';
+$lang['CM_CommentsValidationOn'] = '"Engedélyezés szükséges" opció <b style="color:red;">bekapcsolva</b>';
+$lang['CM_Comments_For_Group'] = 'Csoport tagok hozzászólásainak engedélyezése';
+$lang['CM_Disable'] = 'Kikapcsolva (alapértelmezett)';
+$lang['CM_Enable'] = 'Bekapcsolva';
+$lang['CM_Not_Allowed_Author'] = 'Sajnáljuk, Önnek nincs joga hozzászólások írására. Kérjük lépjen kapcsolatba az adminisztrátorral.';
+$lang['CM_No_Anonymous_Comments'] = 'A hozzászóláshoz a vendégeknek is kötelező felhasználónév megadása';
+$lang['CM_save_config'] = 'Beállítások mentése sikeres!';
+$lang['CM_submit'] = 'Beállítások mentése';
+$lang['CM_SubTitle'] = 'Bővítmény konfiguráció';
+$lang['CM_Title'] = 'Comments Access Manager - Verzió : ';
+?>
