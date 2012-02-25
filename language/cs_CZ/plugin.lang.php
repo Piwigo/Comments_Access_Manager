89f2abff069576444e7f6e7a341c232bf874fb85
ddtddt <ddtddt@mpiwigo.org>
2012-02-25 17:56:52 +0000

    [extensions] - Comments_Access_Manager - add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13357 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+$lang['CM_submit'] = 'Uložit nastavení';
+$lang['CM_save_config'] = 'Konfigurace byla uložena !';
+$lang['CM_Title'] = 'Správa komentářů Access Manager - Verze : ';
+$lang['CM_SubTitle'] = 'Plugin konfigurace';
+$lang['CM_Disable'] = ' Vypnuto (výchozí)';
+$lang['CM_Enable'] = ' Aktivovat ';
+$lang['CM_No_Anonymous_Comments'] = 'Povinná přezdívka pro komentáře neregistrovaných návštěvníků';
+$lang['CM_Comments_For_Group'] = 'Povolit komentáře určité skupině uživatelů';
+$lang['CM_AllowedComm_Group'] = 'Označte skupinu uživatelů, kteří budou mít povolené komentáře:';
+$lang['CM_Not_Allowed_Author'] = 'Omlouváme se, nejste oprávněn/a přidávat komentáře.Prosím kontaktujte administrátora webu.';
+$lang['CM_Support_txt'] = 'Oficiální podpora pro tento pluginje je pouze na tomto vláknu na Piwigo forum:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">AJ fórum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Chybu můžete nahlásit také sem: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Empty Author'] = 'Přezdívka autora komentáře je povinná !';
+
+
+// --------- Starting below: New or revised $lang ---- from version 2.2.1
+$lang['CM_Validation_For_Group'] = 'Povolit přidání komentáře bez ověření administrátorem';
+$lang['CM_ValidComm_Group'] = 'Označte skupinu uživatelů, kteří budou mít povolené komentáře bez ověřování:';
+// --------- End: New or revised $lang ---- from version 2.2.1
+
+
+// --------- Starting below: New or revised $lang ---- from version 2.2.2
+$lang['CM_CommentsForAll'] = '"Komentáře pro všechny" volba je <b style="color:red;">aktivní</b>';
+$lang['CM_CommentsForRegistered'] = '"Komentáře pro všechny" volba je <b style="color:red;">vypnuta</b>';
+// --------- End: New or revised $lang ---- from version 2.2.2
+
+
+// --------- Starting below: New or revised $lang ---- from version 2.2.6
+$lang['CM_CommentsValidationOn'] = '"Ověřování" pro komentáře je <b style="color:red;">aktivní</b>';
+$lang['CM_CommentsValidationOff'] = '"Ověřování" pro komentáře je <b style="color:red;">vypnuto</b>';
+// --------- End: New or revised $lang ---- from version 2.2.6
+?>
