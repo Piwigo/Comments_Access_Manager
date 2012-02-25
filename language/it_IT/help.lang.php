a7dcbe93046246f37a2229b230ee13b4c0161744
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-25 09:29:10 +0000

    [Comments Access Manager] Update it_IT, thanks to : Gdvpixel
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13333 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/help.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Verifica&quot; per i commenti è disabilitata';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Abilita questa opzione per mostrare le impostazioni avanzate';
+$lang['CM_commentTitle'] = 'Nickname obbligatorio per i commenti degli ospiti';
+$lang['CM_commentTitle_d'] = 'Se <b>&quot;Commenti per tutti&quot;</b> è attivato (gli utenti non registrati possono inserire commenti), questa opzione permette di forzare l\'utente non registrato ad inserire un nickname';
+$lang['CM_GroupCommTitle'] = 'Permetti commenti ad un gruppo di utenti';
+$lang['CM_ValidCommTitle'] = 'Permetti commenti senza verifica di alcun amministratore';
+?>
---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Seleziona il gruppo di utenti a cui è permesso inserire commenti';
+$lang['CM_CommentsForAll'] = 'L\'opzione "Commenti per tutti" è <b style="color:red;">enabled</b>';
+$lang['CM_CommentsForRegistered'] = 'L\'opzione "Commenti per tutti" è <b style="color:red;">disabled</b>';
+$lang['CM_CommentsValidationOff'] = 'La verifica dei commenti è <b style="color:red;">disabled</b>';
+$lang['CM_CommentsValidationOn'] = 'La verifica dei commenti è <b style="color:red;">enabled</b>';
+$lang['CM_Comments_For_Group'] = 'Permetti commenti a un gruppo di utenti';
+$lang['CM_Disable'] = 'Disabilita (default)';
+$lang['CM_Empty Author'] = 'Il nickname dell\'autore è obbligatorio';
+$lang['CM_Enable'] = 'Abilita';
+$lang['CM_Not_Allowed_Author'] = 'Mi dispiace, non sei autorizzato a inserire commenti. Contatta un amministratore del sito.';
+$lang['CM_No_Anonymous_Comments'] = 'Nickname obbligatorio per i commenti ospiti';
+$lang['CM_save_config'] = 'Configurazione salvata con successo';
+$lang['CM_submit'] = 'Salva impostazioni';
+$lang['CM_SubTitle'] = 'Configurazione Plugin';
+$lang['CM_Support_txt'] = 'Il supporto ufficiale di questo Plugin è solo sul forum di Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Forum inglese - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Disponibile anche il bugtracker del progetto: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Comments Access Manager - Versione';
+$lang['CM_Validation_For_Group'] = 'Permetti commenti senza verifica di alcun amministratore';
+$lang['CM_ValidComm_Group'] = 'Seleziona il gruppo di utenti a cui è permesso inserire commenti senza verifica';
+?>
