47589957663efb6f08a851a437f9e956d5dde5b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-26 07:42:42 +0000

    [Comments Access Manager] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13363 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Виберіть групу користувачів, допускається залишати коментарі:';
+$lang['CM_CommentsForAll'] = 'Коментарі для всіх <b style="color:red;">увімкнені</b>';
+$lang['CM_CommentsForRegistered'] = 'Коментарі для всіх <b style="color:red;">вимкнені</b>';
+$lang['CM_CommentsValidationOff'] = 'Перевірка коментарів <b style="color:red;">вимкнена</b>';
+$lang['CM_CommentsValidationOn'] = 'Перевірка коментарів <b style="color:red;">увімкнена</b>';
+$lang['CM_Comments_For_Group'] = 'Дозволити коментарі до групи користувачів';
+$lang['CM_Disable'] = 'Відключити (за замовчуванням)';
+$lang['CM_Empty Author'] = 'Ім\'я автора коментарів є обов\'язковим!';
+$lang['CM_Enable'] = 'Увімкнути';
+$lang['CM_Not_Allowed_Author'] = 'На жаль, ви не авторизовані, щоб залишити коментар. Будь ласка, зв\'яжіться з адміністратором сайту.';
+$lang['CM_No_Anonymous_Comments'] = 'Ім\'я є обов\'язковим для коментарів гостей';
+$lang['CM_save_config'] = 'Налаштування успішно збережено!';
+$lang['CM_submit'] = 'Зберегти налаштування';
+$lang['CM_SubTitle'] = 'Конфігурації плаґіна';
+$lang['CM_Support_txt'] = 'Офіційна підтримка цього плаґіну тільки на форумі Piwigo :<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Англійський форум - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Також доступний проект багтрекер: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Менеджер доступу коментарів - Версія:';
+$lang['CM_Validation_For_Group'] = 'Дозволити коментарі без перевірки адміністратором';
+$lang['CM_ValidComm_Group'] = 'Виберіть групу користувачів, які дозволено відправляти коментарі без перевірки:';
+?>
