faa75b7e5335f0abf8d7fc5748e36c15f1613885
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-26 16:24:38 +0000

    [Comments Access Manager] Update de_DE, thanks to : Eric
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13374 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/help.lang.php
+++ language/de_DE/help.lang.php
@@ -25,4 +25,14 @@ $lang['CM_CommentsValidationOffTitle_d'] = 'Diese Option aktivieren, um zu hier 
 $lang['CM_commentTitle'] = 'Nickname obligatorisch für Gäste Kommentare';
 $lang['CM_GroupCommTitle'] = 'Kommentaren zulassen zu einer Benutzern Gruppe';
 $lang['CM_ValidCommTitle'] = 'Kommentare zulassen ohne Validierung durch einen Administrator';
+$lang['CM_commentTitle_d'] = 'Wenn <b>&quot;Kommentare für alle&quot;</b> ist aktiv (Befugnis, unregistrierte Besucher um Kommentare zu schreiben), ist diese Option, um die nicht-registrierte Besucher zu zwingen, einen Spitznamen, dass der Kommentar angenommen wird eingeben können.';
+$lang['CM_GroupCommTitle_d'] = 'Mit dieser Option können Sie eine Gruppe von Nutzern, die gestattet, Kommentare, wenn die Galerie so konfiguriert ist, nicht zulassen, dass für alle Kommentare veröffentlichen wird.
+<br><br>
+Standardmäßig wird, wenn <b>&quot;Kommentare für alle&quot;</b> Option deaktiviert ist, können nur registrierte Benutzer Kommentare zu schreiben. Bei dieser Option können Sie dieses Verhalten, indem Sie eine Nutzergruppe zu beschränken. So dürfen nur registrierte Benutzer und Mitglieder dieser Gruppe Kommentare zu schreiben.';
+$lang['CM_ValidComm2Title_d'] = 'Mit dieser Option können Sie angeben, eine Gruppe von Benutzern, deren Kommentare werden ohne Überprüfung durch den Administrator, wenn die Galerie so konfiguriert ist, <u>erlauben Kommentare zu allen</u> und dass die Validierung von Kommentaren durch einen Administrator erforderlich zur Verfügung gestellt werden.
+<br><br>
+Standardmäßig wird, wenn <b>&quot;Kommentare für alle&quot;</b> und die Validierung der Kommentare aktiviert sind, sind die Kommentare von allen registrierten Benutzern vorbehalten Validierung durch einen Administrator, bevor sie sichtbar auf der Galerie. Mit dieser Option können Sie erlauben den Mitgliedern einer Gruppe Ihrer Wahl, um Kommentare ohne eine solche vorherige Prüfung veröffentlichen ist nicht erforderlich.';
+$lang['CM_ValidCommTitle_d'] = 'Diese Option können Sie eine Gruppe von Benutzern, deren Kommentare werden ohne Überprüfung durch den Administrator, wenn die Galerie zu <u>konfiguriert ist nicht erlaubt Kommentare zu allen</u> und dass die Validierung von Kommentaren durch einen Administrator erforderlich zur Verfügung gestellt werden.
+<br><br>
+Standardmäßig wird, wenn <b>&quot;Kommentare für alle&quot;</b> ist deaktiviert und die Validierung von Kommentaren aktiviert ist, sind die Kommentare von allen registrierten Benutzern vorbehalten Validierung durch einen Administrator, bevor sie sichtbar auf der Galerie. Mit dieser Option können Sie erlauben den Mitgliedern einer Gruppe Ihrer Wahl, um Kommentare ohne eine solche vorherige Prüfung veröffentlichen ist nicht erforderlich.';
 ?>
