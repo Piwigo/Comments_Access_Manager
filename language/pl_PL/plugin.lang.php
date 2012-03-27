b923c2a23880ef6172c07703850b5965ccc70a65
ddtddt <ddtddt@mpiwigo.org>
2012-03-27 20:57:24 +0000

    [extensions] - Comments_Access_Manager - add pl_PL - Polski - Polish - Thanks to kuba
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13775 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+$lang['CM_submit'] = 'Zapisz ustawienia';
+$lang['CM_save_config'] = 'Konfiguracja zapisana!';
+$lang['CM_Title'] = 'Zarządzanie Dostępem do Komentarzy - Wersja: ';
+$lang['CM_SubTitle'] = 'Konfiguracja plugina';
+$lang['CM_Disable'] = ' Wyłącz (domyślne)';
+$lang['CM_Enable'] = ' Uaktywnij ';
+$lang['CM_No_Anonymous_Comments'] = 'Nick obowiązkowy dla komentarzy gości';
+$lang['CM_Comments_For_Group'] = 'Zezwól na komentarzy grupie użytkowników';
+$lang['CM_AllowedComm_Group'] = 'Wybierz grupę użytkowników, którzy mogą wysyłać komentarze:';
+$lang['CM_Not_Allowed_Author'] = 'Nie jesteś upoważniony do umieszczania komentarzy. Skontaktuj się z administratorem strony.';
+$lang['CM_Support_txt'] = 'Oficjalne wsparcie tego plugina znajduje się tylko w tym temacie na forum Piwigo:<br> <a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Forum angielskojęzyczne - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br> Dostępny jest również bugtracker projektu: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Empty Author'] = 'Nick autora komentarza jest obowiązkowy!';
+$lang['CM_Validation_For_Group'] = 'Zezwól na komentarze bez zatwierdzenia przez administratora';
+$lang['CM_ValidComm_Group'] = 'Wybierz grupę użytkowników, którzy mogą wysyłać komentarze bez zatwierdzania przez administratora:';
+$lang['CM_CommentsForAll'] = 'Opcja "komentarze dla wszystkich" jest <b style="color:red;">aktywna</b>';
+$lang['CM_CommentsForRegistered'] = 'Opcja "komentarze dla wszystkich" jest <b style="color:red;">nieaktywna</b>';
+$lang['CM_CommentsValidationOn'] = 'Zatwierdzanie komentarzy jest <b style="color:red;">włączone</b>';
+$lang['CM_CommentsValidationOff'] = 'Zatwierdzanie komentarzy jest <b style="color:red;">wyłączone</b>'; 
+?>
