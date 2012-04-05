0c4a5c698f7fe3227f11d304d5729591c5aef145
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-05 10:09:54 +0000

    [Comments Access Manager] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@13915 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Označenie skupiny používateľov pre povolenie posielať komentáre:';
+$lang['CM_CommentsForAll'] = '"Komentáre pre všetkých" je <b style="color:red;">povolená</b>';
+$lang['CM_CommentsForRegistered'] = '"Komentáre pre všetkých" je <b style="color:red;">zakázaná</b>';
+$lang['CM_CommentsValidationOff'] = '"Kontrola" pre komentáre je <b style="color:red;">zakázaná</b>';
+$lang['CM_CommentsValidationOn'] = '"Kontrola" pre komentáre je <b style="color:red;">povolená</b>';
+$lang['CM_Comments_For_Group'] = 'Povoliť komentáre pre skupinu používateľov';
+$lang['CM_Disable'] = 'Zakázané (predvolené)';
+$lang['CM_Empty Author'] = 'Prezývka autora komentárov je povinná!';
+$lang['CM_Enable'] = 'Povolené';
+$lang['CM_Not_Allowed_Author'] = 'Ospravedlňujeme sa, ale nemáte povolenie poslať komentár. Prosím kontaktujte administrátora.';
+$lang['CM_No_Anonymous_Comments'] = 'Povinná prezývka pre komentáre návštevníkov';
+$lang['CM_save_config'] = 'Konfigurácia úspešne uložená!';
+$lang['CM_submit'] = 'Uložiť nastavenia';
+$lang['CM_SubTitle'] = 'Konfigurácia doplnkov';
+$lang['CM_Support_txt'] = 'Oficiálna podpora pre tento doplnok je len na Piwigo fóre s topikom:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Taktiež dostupné na stránke projektu: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Správca prístupov ku komentárom - Verzia:';
+$lang['CM_Validation_For_Group'] = 'Povoliť komentáre bez kontroly administrátorom';
+$lang['CM_ValidComm_Group'] = 'Označenie skupiny používateľov pre posielanie komentárov bez kontroly administrátorom:';
+?>
