4afd920780ceb09a0b4cd410e2c7cdd20355599c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-09 21:44:11 +0000

    [Comments Access Manager] Add nl_NL, thanks to : pauldaytona
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@14056 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Selecteer de groep gebruikers die commantaar mogen posten';
+$lang['CM_CommentsForAll'] = '"commentaar voor iedereen" optie is <b style="color:red;">ingeschakeld</b>';
+$lang['CM_CommentsForRegistered'] = '"commentaar voor iedereen" optie is <b style="color:red;">uitgeschakeld</b>';
+$lang['CM_CommentsValidationOff'] = 'Goedkeuring voor commentaar is  <b style="color:red;">uitgeschakeld</b>';
+$lang['CM_CommentsValidationOn'] = 'Goedkeuring voor commentaar is  <b style="color:red;">ingeschakeld</b>';
+$lang['CM_Comments_For_Group'] = 'Sta commentaar toe aan een groep gebruikers';
+$lang['CM_Disable'] = 'Uitschakelen(standaard)';
+$lang['CM_Empty Author'] = 'De nickname van de commentaar auteur is verplicht';
+$lang['CM_Enable'] = 'Inschakelen';
+$lang['CM_No_Anonymous_Comments'] = 'Nickname verplicht voor gast commentaren';
+$lang['CM_Not_Allowed_Author'] = 'Sorry, je hebt geen recht om commentaar te posten, contact de beheerder.';
+$lang['CM_SubTitle'] = 'Plugin configuratie';
+$lang['CM_Support_txt'] = 'De officiele ondersteuning voor deze plugin is alleen in dit Piwigo forum topic:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Ook beschikbaar is de project\'s bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Comments Access Manager - versie:';
+$lang['CM_ValidComm_Group'] = 'Selecteer de groep gebruikers die commantaar mogen posten, zonder goedkeuring';
+$lang['CM_Validation_For_Group'] = 'Sta commentaar toe zonder goedkeuring door een administator';
+$lang['CM_save_config'] = 'Configuratie succesvol bewaard';
+$lang['CM_submit'] = 'Bewaar instellingen';
+?>
