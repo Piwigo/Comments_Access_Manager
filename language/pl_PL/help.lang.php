51f20b80b9e1b5f9c165d8ad4cd8a188e0800cea
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-10 07:55:10 +0000

    [Comments Access Manager] Update pl_PL, thanks to : T00kie
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@14072 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/help.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_CommentsValidationOffTitle_d'] = 'Uaktywnij tą opcję aby zobaczyć rozszerzone parametry';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Zatwierdzanie&quot; komentarzy jest nieaktywne';
+$lang['CM_GroupCommTitle_d'] = 'Opcja ta pozwala na na sprecyzowanie grupy użytkowników, którzy będą mogli dodawać komentarze, w przypadku gdy galeria jest skonfigurowana tak aby nie pozwalać na komentarze wszystkim.
+<br><br>
+Domyślnie, kiedy opcja <b>&quot;komentarze dla wszystkich&quot;</b> jest wyłączona';
+$lang['CM_GroupCommTitle'] = 'Pozawala na komentowanie grupie użytkowników';
+$lang['CM_ValidCommTitle'] = 'Pozwala na komentowanie bez zatwierdzenia administratora';
+$lang['CM_commentTitle_d'] = 'Jeśli <b>&quot;Komentarze dla wszystkich&quot;</b> są aktywne (zezwolenie na umieszczanie komentarzy dla niezarejestrowanych użytkowników), ta opcja wymusza na niezarejestrowanych użytkownikach wpisywanie nicka w celu zatwierdzenia komentarza';
+$lang['CM_commentTitle'] = 'Obowiązujący nick dla komentarzy gości';
+?>
