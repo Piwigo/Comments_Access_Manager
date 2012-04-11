00fce6db7ff3d7806b8443faab35e4cea6fedf32
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-11 20:18:38 +0000

    [Comments Access Manager] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@14101 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Selecione o grupo de utilizadores autorizados a colocar comentários';
+$lang['CM_CommentsForAll'] = 'Opção "Commentários para todos" está <b style="color:red;">activada</b>';
+$lang['CM_CommentsForRegistered'] = 'Opção "Commentários para todos" está <b style="color:red;">desactivada</b>';
+$lang['CM_CommentsValidationOff'] = '"Validação" para comentários está <b style="color:red;">desactivada</b>';
+$lang['CM_CommentsValidationOn'] = '"Validação" para comentários está <b style="color:red;">activada</b>';
+$lang['CM_Comments_For_Group'] = 'Permitir comentários ao grupo de utilizadores';
+$lang['CM_Disable'] = 'Desactivado(por defeito)';
+$lang['CM_Empty Author'] = 'O nome de utilizador é o nome de autor nos comentários';
+$lang['CM_Enable'] = 'ActivaDO';
+$lang['CM_No_Anonymous_Comments'] = 'Nome de utilizador necessário nos comentários de visitantes';
+$lang['CM_Not_Allowed_Author'] = 'Desculpe, não está autorizado(a) a fazer comentários. Por favor contacte o Aministrador do sitio.';
+$lang['CM_SubTitle'] = 'Configuração da extenção';
+$lang['CM_Support_txt'] = 'Suporte oficial para esta extenção pode ser consultada no tópico do forum Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Também existente no projecto de bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Gerir acesso aos comentários';
+$lang['CM_ValidComm_Group'] = 'Selecione o grupo de utilizadores aos quais é permitido colocarem comentários sem validação';
+$lang['CM_Validation_For_Group'] = 'Permitir comentários sem validação do Administrador';
+$lang['CM_save_config'] = 'Configuração salva com sucesso!';
+$lang['CM_submit'] = 'Salvar definições';
+?>
