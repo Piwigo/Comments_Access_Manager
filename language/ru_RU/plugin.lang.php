e8aa4ccc6c624130f33bd56115e9ccfa8d67599b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-17 12:16:04 +0000

    [Comments Access Manager] Add ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@14199 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Выберите группу пользователей, которым разрешено отправлять комментарии:';
+$lang['CM_CommentsForAll'] = 'Опция "Комментарии для всех" <b style="color:red;">включена</b>';
+$lang['CM_CommentsForRegistered'] = 'Опция "Комментарии для всех" <b style="color:red;">выключена</b>';
+$lang['CM_CommentsValidationOff'] = '"Проверка" комментариев <b style="color:red;">выключена</b>';
+$lang['CM_CommentsValidationOn'] = '"Проверка" комментариев <b style="color:red;">включена</b>';
+$lang['CM_Comments_For_Group'] = 'Разрешить комментарии группе пользователей';
+$lang['CM_Disable'] = 'Отключено (по умолчанию)';
+$lang['CM_Empty Author'] = 'Никнейм автора комментария обязателен!';
+$lang['CM_Enable'] = 'Включено';
+$lang['CM_No_Anonymous_Comments'] = 'Имя обязательно для комментариев от гостей';
+$lang['CM_Not_Allowed_Author'] = 'К сожалению, вам не разрешено оставлять комментарии. Пожалуйста, свяжитесь с администратором сайта.';
+$lang['CM_SubTitle'] = 'Настройка плагина';
+$lang['CM_Support_txt'] = 'Официальную поддержку этого плагина можно получить только в этих темах форума Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">Английская версия - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Также доступен, багтрекер проекта: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Comments Access Manager - Версия: ';
+$lang['CM_ValidComm_Group'] = 'Выберите группу пользователей, которым разрешено отправлять комментарии без проверки:';
+$lang['CM_Validation_For_Group'] = 'Разрешить комментарии без проверки администратором';
+$lang['CM_save_config'] = 'Конфигурация успешно сохранена!';
+$lang['CM_submit'] = 'Сохранить настройки';
+?>
