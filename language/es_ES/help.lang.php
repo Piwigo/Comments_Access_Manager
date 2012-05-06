32a608570fa9b4ecec9fbb6e960b3700191ec3de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-06 10:12:18 +0000

    [comments_access_manager] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@14794 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/help.lang.php
+++ language/es_ES/help.lang.php
@@ -27,4 +27,12 @@ $lang['CM_GroupCommTitle'] = 'Permite comentar a un grupo de usuarios.';
 $lang['CM_ValidCommTitle'] = 'Permite comentarios sin la aprobación de un administrador';
 $lang['CM_commentTitle_d'] = 'Si Comentar para todos esta activo (autoriza a visitantes sin registro a publicar comentarios), esta opción obliga a los visitantes no registrados a introducir un apodo o nick para que el comentario sea aceptado.';
 $lang['CM_commentTitle'] = 'El Nick o apodo es obligatorio para los comentarios de los visitantes';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Activar esta opción para ver aquí los parámetros extendidos.';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Validacion&quot; para los comentarios está desactivada.';
+$lang['CM_ValidComm2Title_d'] = 'Esta opción le permite especificar un grupo de usuarios cuyos comentarios se publicarán siempre sin la validación por el administrador cuando la galería está configurado para permitir <u> comentarios a todos </ u> y que la validación de los comentarios de un administrador se requiera.
+<br>
+Por defecto, cuando <b> "comentarios para todos" </ b> y la validación de los comentarios están activados, los comentarios de todos los usuarios registrados están sujetos a la validación por un administrador antes de ser visibles en la galería. Con esta opción, puede permitir a los miembros de un grupo de su elección para enviar comentarios sin aprobación previa de este tipo se requiere.';
+$lang['CM_ValidCommTitle_d'] = 'Esta opción le permite especificar un grupo de usuarios cuyos comentarios se publicarán siempre sin la validación por el administrador cuando la galería está configurada para no permitir que ningún <u> comentario se publique automáticamente </ u> y que la validación de estos comentarios requiere moderación de un administrador.
+<br>
+Por defecto, cuando <b> "comentarios para todos" </ b> se desactiva y la validación de los comentarios está activada, los comentarios de todos los usuarios registrados están sujetos a la validación por un administrador antes de ser visibles en la galería. Con esta opción, puede permitir a los miembros de un grupo de su elección para enviar comentarios sin aprobación previa de este tipo se requiere.';
 ?>
