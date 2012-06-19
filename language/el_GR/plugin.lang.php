830d0498aa52b64bca58085ba49b695d04f5f0c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-19 22:56:50 +0000

    [comments_access_manager] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@15842 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/plugin.lang.php
+++ language/el_GR/plugin.lang.php
@@ -23,4 +23,21 @@
 $lang['CM_AllowedComm_Group'] = 'Επιλέξτε την ομάδα των χρηστών που επιτρέπεται να υποβάλουν σχόλια:';
 $lang['CM_CommentsForAll'] = 'Η επιλογή "Σχόλια για όλους" <b style="color:red;"> ενεργοποιημένη </b>';
 $lang['CM_CommentsForRegistered'] = 'Η επιλογή "Σχόλια για όλους" <b style="color:red;"> απενεργοποιημένη </b>';
+$lang['CM_CommentsValidationOff'] = 'Η "Επικύρωση" για τα σχόλια είναι <b style="color:red;">απενεργοποιημένη</b>';
+$lang['CM_CommentsValidationOn'] = 'Η "Επικύρωση" για τα σχόλια είναι <b style="color:red;">ενεργοποιημένη</b>';
+$lang['CM_Comments_For_Group'] = 'Επιτρέψτε τα σχόλια για μια ομάδα χρηστών';
+$lang['CM_Disable'] = 'Απενεργοποιημένο (προεπιλογή)';
+$lang['CM_Empty Author'] = 'Το ψευδώνυμο του δημιουργού του σχολίου είναι υποχρεωτικό!';
+$lang['CM_Enable'] = 'Ενεργοποιημένο';
+$lang['CM_No_Anonymous_Comments'] = 'Το ψευδώνυμο σε σχόλια επισκεπτών είναι υποχρεωτικό!';
+$lang['CM_Not_Allowed_Author'] = 'Συγγνώμη, δεν έχετε το δικαίωμα να δημοσιεύσετε ένα σχόλιο. Παρακαλούμε επικοινωνήστε με τον διαχειριστή.';
+$lang['CM_SubTitle'] = 'Διαμόρφωση Πρόσθετου';
+$lang['CM_Support_txt'] = 'Η επίσημη υποστήριξη γι αυτό το πρόσθετο του Piwigo στο φόρουμ:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Επίσης διαθέσιμο, το σύστημα διαχείρισης αναφορών σφαλμάτων του έργου: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Διαχείριση πρόσβασης Σχολίων - Έκδοση:';
+$lang['CM_ValidComm_Group'] = 'Επιλέξτε την ομάδα χρηστών που επιτρέπεται να υποβάλουν σχόλια χωρίς επικύρωση:';
+$lang['CM_Validation_For_Group'] = 'Αφήστε σχόλια, χωρίς επικύρωση από το διαχειριστή του χώρου';
+$lang['CM_save_config'] = 'Διαμόρφωση αποθηκεύτηκε με επιτυχία!';
+$lang['CM_submit'] = 'Αποθήκευση ρυθμίσεων';
 ?>
