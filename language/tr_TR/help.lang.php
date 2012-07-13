7a85ccb038e936dd28e98bc286dd26c67e725478
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 09:38:49 +0000

    [comments_access_manager] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@16684 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/help.lang.php
+++ language/tr_TR/help.lang.php
@@ -22,8 +22,13 @@
 // +-----------------------------------------------------------------------+
 $lang['CM_CommentsValidationOffTitle_d'] = 'Genişletilmiş seçenekleri burada görmek için bu seçeneği etkinleştirin.';
 $lang['CM_CommentsValidationOffTitle'] = 'Yorumlar için &quot;Doğrulama&quot; devre dışıdır';
-$lang['CM_GroupCommTitle'] = 'Bir kullanıcı grubuna yorumlara izin verir';
+$lang['CM_GroupCommTitle'] = 'Bir kullanıcı grubu yorumlarına izin verir';
 $lang['CM_ValidCommTitle'] = 'Bir yönetici tarafından doğrulama olmadan yorumlara izin verir';
 $lang['CM_commentTitle_d'] = 'Bu seçenek, eğer <b>&quot;Tümü için yorum&quot;</b> seçeneği etkin ise (kayıtlı olmayan ziyaretçilerin yorum gönderebilmesi izni) yorumlarının kabul edilmesi için kayıtlı olmayan ziyaretçileri takma ad girmeleri konusunda zorlar.';
 $lang['CM_commentTitle'] = 'Ziyaretçi yorumları için takma ad zorunludur';
+$lang['CM_GroupCommTitle_d'] = 'Bu seçenek, yorum gönderme izninin herkes için kapalı olarak yapılandırıldığı bir albümde yorum göndermesine izin verilecek
+bir kullanıcı grubu belirlemenizi sağlar.
+<br><br>
+Varsayılan olarak, <b>&quot;herkes için yorum&quot;</b> seçeneği devre dışıyken sadece kayıtlı kullanıcılar yorum gönderebilir.
+Bu seçenek ile, bir kullanıcı grubu seçerek bu davranışı kısıtlayabilirsiniz. Bu nedenle sadece kayıtlı kullanıcılar ve seçilen grup üyeleri yorum gönderebilir.';
 ?>
