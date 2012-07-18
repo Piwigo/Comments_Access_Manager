9532d07417a5969b9486c06c330a13474ce53a73
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-18 17:33:50 +0000

    [comments_access_manager] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@16831 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/help.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_CommentsValidationOffTitle_d'] = 'Iespējot šo opciju, lai šeit redzētu paplašinātos parametrus';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Validācija&quot; komentāriem ir atspējota';
+$lang['CM_GroupCommTitle_d'] = 'Šī opcija dod iespēju noteikt lietotāju grupu, kuriem būs atļauts publicēt komentārus tad, kad galerija būs nokonfigurēta aizliedzot komentārus visiem.
+<br><br>
+Pēc noklusējuma, kad opcija <b>&quot;komentāri visiem&quot;</b> ir atspējota, tikai reģistrētiem lietotājiem būs aļauts publicēt komentārus. Šī opcija dod iespēju noteikt šo kārtību, izveidojot īpašu lietotāju grupu. Tādējādi komentārus drīkstēs publicēt tikai reģistrētie lietotāji un šīs grupas biedri.';
+$lang['CM_GroupCommTitle'] = 'Atļaut komentārus lietotāju grupai';
+$lang['CM_ValidCommTitle'] = 'Atļaut komentārus bez administratora validācijas';
+$lang['CM_commentTitle_d'] = 'Ja opcija <b>&quot;Komentāri visiem&quot;</b> ir aktīva (pilnvaras nereģistrētiem apmeklētājiem nosūtīt komentārus), šī iespēja ļauj nereģistrētiem apmeklētājiem, lietojot atbilstošu segvārdu, veikt komentārus.';
+$lang['CM_commentTitle'] = 'Viesa komentāriem obligāts Segvārs (niks) ';
+?>
