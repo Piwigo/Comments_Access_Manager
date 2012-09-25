5bbb738d422dc780272ce7815dcee6db846566c4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-25 21:09:17 +0000

    [comments_access_manager] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@18250 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/help.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Godkendelse&quot; af kommentarer er deaktiveret';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Aktiv denne valgmulighed, for her at se udvidede parametre';
+$lang['CM_GroupCommTitle'] = 'Giv en gruppe af brugere adgang til kommentere';
+$lang['CM_GroupCommTitle_d'] = 'Valgmuligheden giver dig mulighed for at angive en gruppe af brugere, som har tilladelse til at tilføje kommentarer, når galleriet er opsat til ikke at tillade kommentarer fra alle.
+<br><br>
+Som standard, når valgmuligheden <b>&quot;kommentarer fra alle&quot;</b> er deaktiveret, kan kun registrerede brugere tilføje kommentarer. Med valgmuligheden kan det begrænses ved at angive en brugergruppe. Dermed kan kun registrerede brugere, som er medlem af denne gruppe, tilføje kommentarer.';
+$lang['CM_ValidComm2Title_d'] = 'Valgmuligheden giver dig mulighed for at angive en gruppe af brugere, hvis kommentarer tilføjes uden administratorgodkendelse, når galleriet er opsat til <u>tillad kommentarer fra alle</u> og administratorgodkendelse af kommentarer er krævet.
+<br><br>
+Som standard, når valgmuligheden <b>&quot;kommentarer fra alle&quot;</b> og godkendelse af kommentarer er aktiveret, vil registrerede brugeres kommentarer skulle godkendes af en administrator, før de bliver synlige i galleriet. Med valgmuligheden kan du give medlemmer af en valgfri gruppe mulighed for at tilføje kommentarer uden at en sådan godkendelse er krævet.';
+$lang['CM_ValidCommTitle'] = 'Tillad kommentarer uden administratorgodkendelse';
+$lang['CM_ValidCommTitle_d'] = 'Valgmuligheden giver dig mulighed for at angive en gruppe af brugere, hvis kommentarer vil blive tilføjet uden administratorgodkendelse, når galleriet er opsat til <u>tillad ikke kommentarer fra alle</u> og administratorgodkendelse af kommentarerer er krævet.
+<br><br>
+Som standard, når valgmuligheden <b>&quot;kommentarer fra alle&quot;</b> er deaktiveret og godkendelse af kommentarer er aktiveret, vil kommentarer fra alle registrerede brugere skulle godkendes af en administrator, før de bliver synlige i galleriet. Med valgmuligheden kan du give medlemmer af en valgfri gruppe mulighed for at tilføje kommentarer uden at en sådan godkendelse er nødvendig.';
+$lang['CM_commentTitle'] = 'Nickname krævet ved kommentarer fra gæster';
+$lang['CM_commentTitle_d'] = 'Hvis <b>&quot;kommentarer fra alle&quot;</b> er aktiv (giver ikke-registrerede gæster mulighed for at tilføje kommentarer), tvinger valgmuligheden til at angive et nickname for at kommentaren accepteres.';
+?>
---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_AllowedComm_Group'] = 'Vælg gruppe af brugere som må tilføje kommentarer:';
+$lang['CM_CommentsForAll'] = 'Valgmuligheden "Kommentarer for alle" er <b style="color:red;">aktiveret</b>';
+$lang['CM_CommentsForRegistered'] = 'Valgmuligheden "Kommentarer for alle" er <b style="color:red;">deaktiveret</b>';
+$lang['CM_CommentsValidationOff'] = '"Godkendelse" af kommentarer er <b style="color:red;">deaktiveret</b>';
+$lang['CM_CommentsValidationOn'] = '"Godkendelse" af kommentarer er <b style="color:red;">aktiveret</b>';
+$lang['CM_Comments_For_Group'] = 'Giv en gruppe af brugere mulighed for at kommentere';
+$lang['CM_Disable'] = ' Deaktivér (standard)';
+$lang['CM_Empty Author'] = 'Nickname på kommentarforfatter er krævet!';
+$lang['CM_Enable'] = ' Aktivér';
+$lang['CM_No_Anonymous_Comments'] = 'Nickname er krævet ved kommentarer fra gæster';
+$lang['CM_Not_Allowed_Author'] = 'Beklager, du er ikke autoriseret til at tilføje kommentarer. Kontakt webstedets administrator.';
+$lang['CM_SubTitle'] = 'Opsætning af plugin';
+$lang['CM_Support_txt'] = 'Officiel support vedrørende denne plugin er kun tilgængelig i disse Piwigo-forumemner:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">engelsk forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Også tilgængelig i projektets bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Adgangshåndtering vedr. kommentarer - Version:';
+$lang['CM_ValidComm_Group'] = 'Vælg gruppen af brugere, som må tilføje kommentarer uden godkendelse';
+$lang['CM_Validation_For_Group'] = 'Giv mulighed for at kommentere uden administratorgodkendelse';
+$lang['CM_save_config'] = 'Opsætning gemt!';
+$lang['CM_submit'] = 'Gem indstillinger';
+?>
