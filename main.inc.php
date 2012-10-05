00240ebb88b21552afbee05e8ea4ef0459c5e388
Eric <Eric@piwigo.org>
2012-10-05 20:20:20 +0000

    Next version is 2.4.4 : Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@18485 68402e56-0260-453c-a942-63ccdbb3a9ee

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Comments Access Manager
-Version: 2.4.3
+Version: auto
 Description: Gérer l'accès aux commentaites - Manage comments access
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=545
 Author: Eric
