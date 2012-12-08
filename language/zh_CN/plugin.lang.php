b47c890f6c6d12e6ec0f7db4eba8c9e944c372c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-08 07:38:02 +0000

    [comments_access_manager] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@19328 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_Validation_For_Group'] = '管理员评论无需审核';
+$lang['CM_No_Anonymous_Comments'] = '未注册用户必须填写昵称';
+$lang['CM_Support_txt'] = 'Piwigo 论坛的此主题为本插件的唯一官方支持:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+以及本项目的 bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_AllowedComm_Group'] = '选择允许评论的用户组：';
+$lang['CM_CommentsForAll'] = '"允许所有人评论"已<b style="color:red;">启用</b>';
+$lang['CM_CommentsForRegistered'] = '"允许所有人评论"已<b style="color:red;">禁用</b>';
+$lang['CM_CommentsValidationOff'] = '"评论审核"已<b style="color:red;">禁用</b>';
+$lang['CM_CommentsValidationOn'] = '"评论审核"已<b style="color:red;">启用</b>';
+$lang['CM_Comments_For_Group'] = '对某一组用户允许评论';
+$lang['CM_Disable'] = '禁用（默认）';
+$lang['CM_Empty Author'] = '评论人昵称为必填项！';
+$lang['CM_Enable'] = '启用';
+$lang['CM_Not_Allowed_Author'] = '抱歉，您未被允许发表评论，请与站长联系。';
+$lang['CM_SubTitle'] = '插件设置';
+$lang['CM_Title'] = 'Comments Access Manager - 版本：';
+$lang['CM_ValidComm_Group'] = '选择允许评论且无需审核的用户组：';
+$lang['CM_save_config'] = '设置已保存！';
+$lang['CM_submit'] = '保存设置';
+?>
