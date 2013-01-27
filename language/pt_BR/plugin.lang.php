2be5926a344fcf6e692ba1290b9796f92ab7d341
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 13:56:18 +0000

    [comments_access_manager] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@20422 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_ValidCommTitle_d'] = 'Esta opção permite que você especifique um grupo de usuários cujos comentários serão dispensados ​​da validação do administrador quando a galeria estiver configurada para <u>não permitir comentários a todos</u> e validação de comentários por um administrador é necessária.
+<br><br>
+Por padrão, quando <b>"comentários para todos"</b> estão desativados e comentários de validação é ativado, os comentários de todos os utilizadores registados são submetidos à validação por um administrador antes de serem exibidas na galeria. Com esta opção, você pode permitir que os membros de um grupo possam postar comentários sem essa autorização prévia.';
+$lang['CM_ValidComm_Group'] = 'Selecione o grupo de usuários habilitados a postar comentários sem validação:';
+$lang['CM_Validation_For_Group'] = 'Permitir comentários sem validação por um administrador';
+$lang['CM_commentTitle'] = 'Apelido obrigatória para comentários de visitantes não registrados ';
+$lang['CM_commentTitle_d'] = 'Se <b>"Comentários para todos"</b> está ativada, esta opção irá forçar o visitantenão registrado a entrar um apelido para este comentário.';
+$lang['CM_save_config'] = 'Configuração salva com sucesso!';
+$lang['CM_submit'] = 'Salvar configurações';
+$lang['CM_AllowedComm_Group'] = 'Selecione o grupo de usuários com permissão para postar comentários:';
+$lang['CM_CommentsForAll'] = '"Comentários para todos" a opção está <b style="color:red;">habilitado</ b>';
+$lang['CM_CommentsForRegistered'] = '"Comentários para todos" a opção está <b style="color:red;">desabilitado</ b>';
+$lang['CM_CommentsValidationOff'] = '"Validação" para comentários está <bstyle="color:red;">desativado</b>';
+$lang['CM_CommentsValidationOffTitle'] = '"Validação" para comentários está desabilitada';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Habilitar a opção de acessar as configurações avançadas.';
+$lang['CM_CommentsValidationOn'] = '"Validação" para comentários está <bstyle="color:red;">habilitada</b>';
+$lang['CM_Comments_For_Group'] = 'Permitir comentários para um grupo de usuários';
+$lang['CM_Disable'] = 'Desativar(padrão)';
+$lang['CM_Empty Author'] = 'O apelido do autor para comentários é obrigatório!';
+$lang['CM_Enable'] = 'Habilitar';
+$lang['CM_GroupCommTitle'] = 'Permitir comentários para um grupo de usuários';
+$lang['CM_GroupCommTitle_d'] = 'Esta opção permite que você especifique um grupo de usuários que terão permissão para enviar comentários quando a galeria estiver configurada para não permitir comentários a todos.
+<br><br>
+Por padrão, quando <b>"comentários para todos"</b> estão desativados, somente usuários registrados podem postar comentários. Com esta opção, você pode restringir esse comportamento especificando um grupo de usuários. Assim, somente usuários registrados e membros do grupo podem enviar comentários.';
+$lang['CM_No_Anonymous_Comments'] = 'Apelido obrigatório para comentários de convidados';
+$lang['CM_Not_Allowed_Author'] = 'Desculpe, você não está autorizado a postar um comentário. Por favor, contate o administrador do site.';
+$lang['CM_SubTitle'] = 'Configuração do plugin';
+$lang['CM_Support_txt'] = 'O suporte oficial sobre este plugin existe apenas neste tópico do fórum Piwigo: <br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a><br><br>
+Also available, the project\'s bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_Title'] = 'Comentários Gerenciador de Acesso - Versão:';
+$lang['CM_ValidComm2Title_d'] = 'Esta opção permite que você especifique um grupo de usuários cujos comentários serão dispensados ​​mediante a validação do administrador quando a galeria estiver configurada para <u>permitir comentários a todos</u> e validação de comentários por um administrador é necessária.
+<br><br>
+Por padrão, quando <b>"comentários para todos"</b> e de validação e comentários são ambos habilitados, os comentários de todos os usuários registados são submetidos à validação por um administrador antes de serem exibidos na galeria. Com esta opção, você pode permitir que os membros do grupo possam postar comentários sem essa autorização prévia.';
+$lang['CM_ValidCommTitle'] = 'Permitir comentários sem a aprovação por um administrador';
+?>
