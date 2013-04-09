84f1853325e06bb24adef223d21308b529c676fa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-09 17:55:48 +0000

    [comments_access_manager] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@22093 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_SubTitle'] = '插件的設定';
+$lang['CM_Title'] = '評論訪問管理器 - 版本：';
+$lang['CM_AllowedComm_Group'] = '請選擇該組允許使用者發表評論：';
+$lang['CM_CommentsForAll'] = '"所有評論"的選項是<b style="color:red;">啟用</b>';
+$lang['CM_CommentsForRegistered'] = '"所有評論"的選項是<b style="color:red;">停用</b>';
+$lang['CM_CommentsValidationOff'] = '“驗證”於評論是<b style="color:red;">停用</b>';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;驗證&quot; 於評論是停用';
+$lang['CM_CommentsValidationOffTitle_d'] = '啟用存取進階設定的選項。';
+$lang['CM_CommentsValidationOn'] = '“驗證”於評論是<b style="color:red;">啟用</b>';
+?>
