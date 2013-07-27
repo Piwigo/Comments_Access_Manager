2df95d1f632bfd43ebf838353e34f6b2beeb358d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-27 05:55:03 +0000

    [comments_access_manager] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@24042 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -42,4 +42,17 @@ $lang['CM_GroupCommTitle'] = '允許的群組使用者群組的評論';
 $lang['CM_ValidCommTitle'] = '允許未經批准的評論';
 $lang['CM_Validation_For_Group'] = '允許未經驗證的評論';
 $lang['CM_commentTitle'] = '未註冊的訪客意見暱稱必須的';
+$lang['CM_Support_txt'] = '這個插件的官方支持，只有Piwigo的論壇主題:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="win​​dow.open(this.href);return false;">English forum - http://piwigo.org /forum/viewtopic.php?id=17577</a><br><br>
+以及本項目的bugtracker: <a href="http://piwigo.org/bugs/" onclick="win​​dow.open(this.href);return false;">http://piwigo.org/bugs/< /a>';
+$lang['CM_GroupCommTitle_d'] = '當允許所有人評論被禁用時，本選項使您能夠指定一組用戶，允許他們發布評論。
+<br><br>
+根據默認設置，當<b>"允許所有人評論"</b> 選項被禁用時，僅註冊用戶可以發布評論。而通過本選項，您可以​​通過指定用戶組的方式對此規則作出進一步限定。即只有註冊用戶並且屬於某個組的用戶方可發布評論。';
+$lang['CM_ValidComm2Title_d'] = '本選項使您能夠指定一組用戶，當相冊被設置為<u>允許所有人評論</u> 且評論需要管理員審核時，允許他們的評論不須管理員審核即可發布。
+<br><br>
+根據默認設置，當<b>"允許所有人評論"</b> 和評論審核功能被啟用時，註冊用戶發布的評論在被相冊顯示前，將提交管理員進行審核。而通過本選項，您可以​​選定某一組的註冊用戶，允許他們的評論無需預先通過審核就能發布。';
+$lang['CM_ValidCommTitle_d'] = '本選項使您能夠指定一組用戶，當<u>允許所有人評論</u> 被禁用且評論需要管理員審核時，允許他們的評論不須管理員審核即可發布。
+<br><br>
+根據默認設置，當<b>"允許所有人評論"</b> 被禁用且評論審核功能被啟用時，註冊用戶發布的評論在被相冊顯示前，將提交管理員進行審核。而通過本選項，您可以​​選定某一組的註冊用戶，允許他們的評論無需預先通過審核就能發布。';
+$lang['CM_commentTitle_d'] = '如果<b>"允許所有人評論"</b> 被啟用（授權未註冊的訪客發布評論），本選項將要求未註冊的訪客在發布評論時填寫一個暱稱。';
 ?>
