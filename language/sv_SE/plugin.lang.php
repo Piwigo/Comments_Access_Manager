5bde8e18d64f315d323798a9b19563a76e98e98f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-11 15:32:13 +0000

    [comments_access_manager] Add sv_SE, thanks to : kabo
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@24151 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_ValidCommTitle_d'] = 'Detta alternativ låter dej specificera en grupp användare vars kommentarer inte behöver valideras av en administratör när galleriet är konfigurerat att <u>inte alla får kommentera</u> och kommentarer måste valideras av en administratör.
+<br /><br />
+Normalt, när <b>&quot;Alla får kommentera&quot;</b> är avstängt och kommentarsvalidering är aktiverat, skickas alla kommendarer från registrerade användare till validering av en administratör innan de visas i galleriet. Med det här alternativet kan du låta en grupp medlemmar kommentera direkt utan att behöva valideras.';
+$lang['CM_ValidComm2Title_d'] = 'Detta alternativ låter dej specificera en grupp användare vars kommentarer inte behöver valideras av en administratör när galleriet är konfigurerat att <u>Alla får kommentera</u> och kommentarer måste valideras av en administratör.
+<br /><br />
+Normalt, när både <b>&quot;Alla får kommentera&quot;</b> och kommentarsvalidering är aktiverat, skickas alla kommendarer till validering av en administratör innan de visas i galleriet. Med det här alternativet kan du låta en grupp medlemmar kommentera direkt utan att behöva valideras.';
+$lang['CM_Support_txt'] = 'Officiell support för det här pluginet finns på Piwigo-forumet:<br />
+<a href="http://piwigo.org/forum/viewtopic.php?id=17577" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=17577</a>
+<br /><br />
+Projektets bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['CM_GroupCommTitle_d'] = 'Detta alternativ låter dej specificera en grupp användare som får kommentera när galleriet är konfigurerat att inte låta alla kommentera
+<br /><br />
+Normalt, när <b>&quot;Alla får kommentera&quot;</b> är avstängt, får bara registrerade användare kommentera. Med det här alternativet kan du begränsa ytterligare så att bara registrerade användare som är med i en specifik grupp kan kommentera.';
+$lang['CM_commentTitle_d'] = 'Om <b>&quot;Alla får kommentera&quot;</b> är aktiverat kommer detta alternativ att göra alias obligatoriskt för icke-registrerade kommentatorer.';
+$lang['CM_Validation_For_Group'] = 'Tillåt kommentarer utan validering av en administratör';
+$lang['CM_commentTitle'] = 'Alias obligatoriskt för icke-registrerade kommentatorer';
+$lang['CM_save_config'] = 'Konfigurationen sparades!';
+$lang['CM_submit'] = 'Spara inställningar';
+$lang['CM_AllowedComm_Group'] = 'Välj gruppen användare som får kommentera:';
+$lang['CM_CommentsForAll'] = '"Alla får kommentera" är <b style="color:red;">aktiverat</b>';
+$lang['CM_CommentsForRegistered'] = '"Alla får kommentera" är <b style="color:red;">avstängt</b>';
+$lang['CM_CommentsValidationOff'] = '"Validering" av kommentarer <b style="color:red;">avstängt</b>';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Validering&quot; av kommentarer är avstängt';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Aktivera alternativet för att komma åt avancerade inställningar.';
+$lang['CM_CommentsValidationOn'] = '"Validering" av kommentarer <b style="color:red;">aktiverat</b>';
+$lang['CM_Comments_For_Group'] = 'Tillåt en grupp användare att kommentera';
+$lang['CM_Empty Author'] = 'Kommentarsförfattarens alias är obligatoriskt!';
+$lang['CM_GroupCommTitle'] = 'Tillåt en användargrupp att kommentera';
+$lang['CM_No_Anonymous_Comments'] = 'Alias är obligatoriskt för gäst-kommentarer';
+$lang['CM_Not_Allowed_Author'] = 'Tyvärr, du är inte auktoriserad att kommentera. Kontakta administratören.';
+$lang['CM_SubTitle'] = 'Publik konfiguration';
+$lang['CM_Title'] = 'Comments Access Manager - Version : ';
+$lang['CM_ValidCommTitle'] = 'Tillåt kommentarer utan godkännande av en administratör';
+$lang['CM_ValidComm_Group'] = 'Välj den grupp användare som får kommentera utan validering:';
+$lang['CM_Disable'] = 'Avstängd (utgångsläget)';
+$lang['CM_Enable'] = 'Aktiverad';
+?>
