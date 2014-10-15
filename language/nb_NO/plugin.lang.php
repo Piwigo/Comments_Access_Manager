a7c5ba03d32a48dc962a58264fc5fae572ebbae6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-15 19:19:59 +0000

    [comments_access_manager] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Comments_Access_Manager@30089 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CM_GroupCommTitle_d'] = 'Dette alternativet tillater deg å angi en spesifikk gruppe av brukere som vil få lov til å skrive kommentarer når galleriet er konfigurert til ikke å tillate kommentarer fra alle.<br><br>
+Som standard, når <b>&quot; kommentarer for alle&quot;</b> er deaktivert, kun registrerte brukere kan skrive kommentarer. Med dette alternativet kan du begrense dette problemet ved å angi en brukergruppe. Dermed kan bare registrerte brukere og medlem av gruppen skrive kommentarer.';
+$lang['CM_AllowedComm_Group'] = 'Velg en gruppen av brukere som har lov til å skrive kommentarer:';
+$lang['CM_CommentsForAll'] = '"Kommentarer tillat for alle" alternativet er <b style = "farge:rød;">aktiver</b>';
+$lang['CM_CommentsForRegistered'] = '"Kommentarer tillat for alle" alternativet er <b style = "farge:rød;">deaktiver</b>';
+$lang['CM_CommentsValidationOff'] = '"Godkjenning" av kommentarer er <b style = "farge:rød;"> deaktivert</b>';
+$lang['CM_CommentsValidationOffTitle'] = '&quot;Godkjenning&quot; av kommentarer er deaktivert';
+$lang['CM_CommentsValidationOffTitle_d'] = 'Aktiver muligheten til å få tilgang til avanserte innstillinger.';
+$lang['CM_CommentsValidationOn'] = '"Godkjenning" av kommentarer er <b style = "farge:rød;"> aktivert</b>';
+$lang['CM_Comments_For_Group'] = 'Tillater kommentarer fra en gruppe av brukere';
+$lang['CM_Disable'] = 'Deaktivert(standard)';
+$lang['CM_Empty Author'] = 'Forfatters kallenavn er obligatorisk i kommentaren!';
+$lang['CM_Enable'] = 'Aktivert';
+$lang['CM_GroupCommTitle'] = 'Tillat kommentarer fra en bruker gruppe';
+$lang['CM_No_Anonymous_Comments'] = 'Kallenavn er obligatorisk for gjeste kommentarer';
+$lang['CM_Not_Allowed_Author'] = 'Beklager, du har ikke tillatelse til å skrive kommentarer. Vennligst kontakt sidens administrator.';
+$lang['CM_SubTitle'] = 'Konfigurasjon av programtillegget';
+$lang['CM_Title'] = 'Kommentarers Tilgangs Manager - Versjon:';
+$lang['CM_ValidCommTitle'] = 'Tillat kommentarer uten godkjenning av en administrator';
